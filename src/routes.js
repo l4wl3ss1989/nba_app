a0e91284ea90a4fd3778061b519e0ba4f365ba72
Didac <didac@compsaonline.com>
2019-02-19 10:24:19 +0100

    1rst basic part done

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ import Layout from './hoc/Layout/layout';
 
 import NewsArticle from './components/Articles/News/Post/index';
 import VideoArticle from './components/Articles/Videos/Video/index';
+import NewsMain from './components/Articles/News/Main/index';
+import VideosMain from './components/Articles/Videos/Main/index';
 
 class Routes extends Component {
     render() {
@@ -13,8 +15,10 @@ class Routes extends Component {
             <Layout>
                 <Switch>
                     <Route path="/" exact component={Home} />
+                    <Route path="/news" exact  component={NewsMain}/>
+                    <Route path="/videos" exact  component={VideosMain}/>
                     <Route path="/articles/:id" exact  component={NewsArticle}/>
-                    <Route path="/videos/:id" exact  component={VideoArticle}/>
+                    <Route path="/videos/:id" exact  component={VideoArticle}/>                    
                 </Switch>
             </Layout>
             
